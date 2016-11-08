f8e03e53159487a82b95bd917a4b2c29b39797cd
Alex Efremov <alex.efremov.dev@gmail.com>
2016-11-08 21:46:26 +0200

    Redirect to original link when visit short url

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(express.static(publicPath));
 
-app.get('/new/:url(*)', function (req, res) {
+app.get('/new/:url(*)', (req, res) => {
   const longUrl = req.params.url;
 
   if(validator.isURL(longUrl)) {
@@ -30,7 +30,7 @@ app.get('/new/:url(*)', function (req, res) {
 
       db.collection('Links').insertOne(newLink).then((result) => {
         console.log('Link was successfully added');
-        res.send(longUrl);
+        res.send(shortUrl);
       }, (err) => {
         console.log('Unable to insert link');
       });
@@ -40,8 +40,29 @@ app.get('/new/:url(*)', function (req, res) {
   } else {
     res.send({error: 'Wrong url format, make sure you have a valid protocol and real site.'});
   }
+});
 
+app.get('/:short', (req, res) => {
+  const shortUrl = req.params.short;
 
+  MongoClient.connect(mongoUri, (err, db) => {
+    if(err) {
+      return console.log('Unable to connect to Mongodb server');
+    }
+    console.log('Connected to MongoDB server');
+
+    db.collection('Links').findOne({short: shortUrl}).then((doc) => {
+      if(doc) {
+        res.redirect(doc.url);
+      } else {
+        res.send({error: 'This url is not on the database.'});
+      }
+    }, (err) => {
+      console.log('Unable to fetch data');
+    });
+
+    db.close();
+  });
 });
 
 app.listen(port, () => {
